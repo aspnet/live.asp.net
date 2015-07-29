2fae338678acfe018f280d525da9cc6d2a4d10cf
damianedwards <damian@damianedwards.com>
2015-07-28 23:33:49 -0700

    Added a countdown to the home page: - Factored JS out into site.js - Created a Tag Helper to inline the JS into the HTML

---
+++ src/live.asp.net/TagHelpers/ScriptInliningTagHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.FileProviders;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Razor;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace live.asp.net.TagHelpers
+{
+    [TargetElement("script", Attributes = "inline")]
+    public class ScriptInliningTagHelper : TagHelper
+    {
+        private readonly IFileProvider _wwwroot;
+
+        public ScriptInliningTagHelper(IHostingEnvironment env)
+        {
+            _wwwroot = env.WebRootFileProvider;
+        }
+
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public bool Inline { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!Inline)
+            {
+                return;
+            }
+
+            var src = output.Attributes["src"];
+
+            var path = src?.Value.ToString();
+            if (path == null)
+            {
+                return;
+            }
+
+            var resolvedPath = path;
+            var queryStringStartIndex = path.IndexOf('?');
+            if (queryStringStartIndex != -1)
+            {
+                resolvedPath = path.Substring(0, queryStringStartIndex);
+            }
+
+            Uri uri;
+            if (Uri.TryCreate(resolvedPath, UriKind.Absolute, out uri))
+            {
+                // Don't inline if the path is absolute.
+                return;
+            }
+
+            var fileInfo = _wwwroot.GetFileInfo(resolvedPath);
+            var requestPathBase = ViewContext.HttpContext.Request.PathBase;
+            if (!fileInfo.Exists)
+            {
+                if (requestPathBase.HasValue &&
+                    resolvedPath.StartsWith(requestPathBase.Value, StringComparison.OrdinalIgnoreCase))
+                {
+                    resolvedPath = resolvedPath.Substring(requestPathBase.Value.Length);
+                    fileInfo = _wwwroot.GetFileInfo(resolvedPath);
+                }
+
+                if (!fileInfo.Exists)
+                {
+                    // Don't inline if the file is not in the current server
+                    return;
+                }
+            }
+
+            using (var readStream = fileInfo.CreateReadStream())
+            using (var reader = new StreamReader(readStream, Encoding.UTF8))
+            {
+                output.Content.Append(reader.ReadToEnd());
+            }
+
+            output.Attributes.Remove(src);
+        }
+    }
+}
--- src/live.asp.net/wwwroot/js/site.js
+++ src/live.asp.net/wwwroot/js/site.js
@@ -1,1 +1,130 @@
-﻿// Write your Javascript code.
+﻿(function (window, undefined) {
+    "use strict";
+
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    function monthName(month) {
+        return months[month];
+    }
+
+    function formatTime(hours, minutes) {
+        var tt = hours < 12 ? " AM" : " PM",
+            hour = hours < 12
+                ? hours
+                : hours === 12
+                    ? 12
+                    : (hours - 12),
+            mins = minutes < 10 ? "0" + minutes : minutes;
+
+        return hour + ":" + mins + tt;
+    }
+
+    function formatDateTime(dateTime) {
+        return dateTime.getDate() + " " +
+               monthName(dateTime.getMonth()) + ", " +
+               dateTime.getFullYear() + " at " +
+               formatTime(dateTime.getHours(), dateTime.getMinutes());
+    }
+
+    function extractTimeZoneName(dateString) {
+        var name = dateString.substring(dateString.indexOf("(") + 1);
+        name = name.substr(0, name.length - 1);
+        return name;
+    }
+
+    function dateDiff(date1, date2) {
+        var ms = date2 - date1,
+            totalSecs, totalMins, totalHrs, totalDays, totalWeeks,
+            weeks, days, hrs, mins, secs;
+
+        totalSecs = ms / 1000;
+        totalMins = totalSecs / 60;
+        totalHrs = totalMins / 60;
+        totalDays = totalHrs / 24;
+        totalWeeks = totalDays / 7;
+
+        weeks = Math.trunc(totalWeeks);
+        days = Math.trunc(totalDays - (weeks * 7));
+        hrs = Math.trunc(totalHrs - (days * 24) - (weeks * 7 * 24));
+        mins = Math.trunc(totalMins - (hrs * 60) - (days * 24 * 60) - (weeks * 7 * 24 * 60));
+        secs = Math.trunc(totalSecs - (mins * 60) - (hrs * 60 * 60) - (days * 24 * 60 * 60) - (weeks * 7 * 24 * 60 * 60));
+
+        return {
+            secs: secs,
+            mins: mins,
+            hrs: hrs,
+            days: days,
+            weeks: weeks,
+            totalSecs: totalSecs,
+            totalMins: totalMins,
+            totalHrs: totalHrs,
+            totalDays: totalDays,
+            totalWeeks: totalWeeks,
+            toString: function () {
+                return weeks + " weeks, " + days + " days, " + hrs + " hours, " + mins + " mins, " + secs + " secs";
+            },
+            toShortString: function () {
+                return (weeks > 0 ? weeks + "w " : "") +
+                       (days > 0 ? days + "d " : "") +
+                       (hrs > 0 ? hrs + "h " : "") +
+                       (mins > 0 ? mins + "m " : "") +
+                       secs + "s";
+            }
+        }
+    }
+
+    function countdownTo(futureDate, tickCallback, endCallback) {
+        var interval = window.setInterval(function () {
+            var now = new Date(),
+                diff = dateDiff(now, futureDate);
+
+            if (diff.totalSecs < 0) {
+                //window.console.log("Clearing interval");
+                window.clearInterval(interval);
+                endCallback();
+                return;
+            }
+
+            tickCallback(diff);
+        }, 500);
+    }
+
+    function data(el, name) {
+        return el.getAttribute("data-" + name);
+    }
+
+    function getNextShowTime(el) {
+        return new Date(Date.UTC(
+            data(el, "utc-year"),
+            data(el, "utc-month"),
+            data(el, "utc-day"),
+            data(el, "utc-hour"),
+            data(el, "utc-min"),
+            data(el, "utc-sec")));
+    }
+
+    window.siteJs = {
+        setNextShowDetails: function (elementId) {
+            // Get the show details
+            var countdownEl,
+                showDetailsEl = window.document.getElementById(elementId),
+                showTimeUtc = getNextShowTime(showDetailsEl),
+
+            // Set the show time display
+            showTimeEl = showDetailsEl.querySelector("[data-part='showTime']"),
+            timeZoneName = extractTimeZoneName(showTimeUtc.toString());
+
+            showTimeEl.textContent = formatDateTime(showTimeUtc) + " (" + timeZoneName + ")";
+            showTimeEl.className = "";
+
+            // Start the countdown
+            countdownEl = showDetailsEl.querySelector("[data-part='countdown']");
+            countdownTo(showTimeUtc, function (diff) {
+                countdownEl.textContent = diff.toShortString();
+            }, function () {
+                countdownEl.textContent = "";
+            });
+        }
+    };
+
+})(window);
