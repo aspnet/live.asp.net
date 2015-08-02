6a88fdd6b71e71cbb446f071cc572601f33929d5
damianedwards <damian@damianedwards.com>
2015-08-01 22:51:29 -0700

    Tweaks to iCalenderOutputFormatter & home page button - #18

--- src/live.asp.net/Formatters/iCalendarOutputFormatter.cs
+++ src/live.asp.net/Formatters/iCalendarOutputFormatter.cs
@@ -28,7 +28,7 @@ namespace live.asp.net.Formatters
         {
             var liveShowDetails = context.Object as LiveShowDetails;
 
-            Debug.Assert(liveShowDetails != null, "Object to be formatted should be of type LiveShowDetails");
+            Debug.Assert(liveShowDetails != null, $"Object to be formatted should be of type {nameof(LiveShowDetails)}");
 
             return WriteLiveShowDetailsToResponseBody(liveShowDetails, context.HttpContext.Response.Body);
         }
@@ -40,6 +40,8 @@ namespace live.asp.net.Formatters
             "UID:aspnet@microsoft.com" + Environment.NewLine +
             "DTSTART:";
 
+        private static string _iCalMiddle = Environment.NewLine + "DTEND:";
+
         private static string _iCalFooter =
             Environment.NewLine +
             "SUMMARY:ASP.NET Community Standup" + Environment.NewLine +
@@ -49,13 +51,14 @@ namespace live.asp.net.Formatters
             "END:VCALENDAR";
 
         private static byte[] _iCalHeaderBytes = Encoding.UTF8.GetBytes(_iCalHeader);
+        private static byte[] _iCalMiddleBytes = Encoding.UTF8.GetBytes(_iCalMiddle);
         private static byte[] _iCalFooterBytes = Encoding.UTF8.GetBytes(_iCalFooter);
 
         private static string _dateTimeFormat = "yyyyMMddTHHmmssZ";
 
         private static int _footerOffset =
             _iCalHeaderBytes.Length +
-            Encoding.UTF8.GetByteCount(_dateTimeFormat + Environment.NewLine + "DTEND:" + _dateTimeFormat);
+            Encoding.UTF8.GetByteCount(_dateTimeFormat + _iCalMiddle + _dateTimeFormat);
 
         private static int _payloadLength = _footerOffset + _iCalFooterBytes.Length;
 
@@ -73,14 +76,15 @@ namespace live.asp.net.Formatters
                END:VEVENT
                END:VCALENDAR */
 
-            var payload = Encoding.UTF8.GetBytes(
-                liveShowDetails.NextShowDateUtc.Value.ToString(_dateTimeFormat) + Environment.NewLine + "DTEND:" +
-                (liveShowDetails.NextShowDateUtc.Value.AddMinutes(30)).ToString(_dateTimeFormat));
+            var start = Encoding.UTF8.GetBytes(liveShowDetails.NextShowDateUtc.Value.ToString(_dateTimeFormat));
+            var end = Encoding.UTF8.GetBytes(liveShowDetails.NextShowDateUtc.Value.AddMinutes(30).ToString(_dateTimeFormat));
 
             var responseBuffer = new byte[_payloadLength];
 
             Buffer.BlockCopy(_iCalHeaderBytes, 0, responseBuffer, 0, _iCalHeaderBytes.Length);
-            Buffer.BlockCopy(payload, 0, responseBuffer, _iCalHeaderBytes.Length, payload.Length);
+            Buffer.BlockCopy(start, 0, responseBuffer, _iCalHeaderBytes.Length, start.Length);
+            Buffer.BlockCopy(_iCalMiddleBytes, 0, responseBuffer, _iCalHeaderBytes.Length + start.Length, _iCalMiddleBytes.Length);
+            Buffer.BlockCopy(end, 0, responseBuffer, _iCalHeaderBytes.Length + start.Length + _iCalMiddleBytes.Length + end.Length, end.Length);
             Buffer.BlockCopy(_iCalFooterBytes, 0, responseBuffer, _footerOffset, _iCalFooterBytes.Length);
 
             return body.WriteAsync(responseBuffer, 0, responseBuffer.Length);
