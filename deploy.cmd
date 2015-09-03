abcd6118ea218df9e8222d2dd50f99bb24f6e21b
Damian Edwards <damian@damianedwards.com>
2015-09-03 12:33:53 -0700

    Update deploy.cmd to use beta7

--- deploy.cmd
+++ deploy.cmd
@@ -81,7 +81,7 @@ IF "%DEPLOYMENT_TARGET:~-1%"=="\" (
 
 :: 1. Set DNX Path
 set DNVM_CMD_PATH_FILE="%USERPROFILE%\.dnx\temp-set-envvars.cmd"
-set DNX_RUNTIME="%USERPROFILE%\.dnx\runtimes\dnx-CLR-win-x86.1.0.0-beta6"
+set DNX_RUNTIME="%USERPROFILE%\.dnx\runtimes\dnx-CLR-win-x86.1.0.0-beta7"
 
 :: 2. Install DNX
 call :ExecuteCmd PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';$CmdPathFile='%DNVM_CMD_PATH_FILE%';& '%SCM_DNVM_PS_PATH%' " install 1.0.0-beta6 -arch x86 -r CLR %SCM_DNVM_INSTALL_OPTIONS%
