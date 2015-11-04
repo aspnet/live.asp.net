39b882be4c0658b5ac0debcd9dd03cc20919bdbe
Bart Calixto <bart@bartmax.com>
2015-11-04 18:20:54 -0300

    Replace Framework and Environment info from inject to Tag Helpers because Tag Helpers are cool!

---
+++ src/live.asp.net/TagHelpers/HtmlCommentTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace live.asp.net.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes ="commented")]
+    public class HtmlCommentTagHelper : TagHelper
+    {
+        private const string StartHtmlComment = "<!-- ";
+        private const string EndHtmlComment = " -->";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.PreContent.AppendEncoded(StartHtmlComment);
+            output.PostContent.AppendEncoded(EndHtmlComment);
+            output.TagName = null;
+        }
+    }
+}
