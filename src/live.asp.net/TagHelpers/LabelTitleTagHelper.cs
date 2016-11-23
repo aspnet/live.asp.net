f411a8fb3f8373e135d18d94650b164a39f7831d
DamianEdwards <damian@damianedwards.com>
2016-11-23 13:41:56 -0800

    Added Tag Helper that sets label title based on model metadata Description

---
+++ src/live.asp.net/TagHelpers/LabelTitleTagHelper.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved. 
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace live.asp.net.TagHelpers
+{
+    [HtmlTargetElement("label", Attributes = "asp-for")]
+    public class LabelTitleTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression For { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var description = For.Metadata.Description;
+            if (!string.IsNullOrEmpty(description) && !output.Attributes.ContainsName("title"))
+            {
+                output.Attributes.Add(new TagHelperAttribute("title", description));
+            }
+        }
+    }
+}
--- src/live.asp.net/ViewModels/AdminViewModel.cs
+++ src/live.asp.net/ViewModels/AdminViewModel.cs
@@ -9,15 +9,15 @@ namespace live.asp.net.ViewModels
 {
     public class AdminViewModel
     {
-        [Display(Name = "Streaming Embed URL", Description = "URL for embedding the live show")]
+        [Display(Name = "Live Show Embed URL", Description = "URL for embedding the live show")]
         [DataType(DataType.Url)]
         public string LiveShowEmbedUrl { get; set; }
         
-        [Display(Name = "Streaming HTML", Description = "HTML content for the live show")]
+        [Display(Name = "Live Show HTML", Description = "HTML content for the live show")]
         [DataType(DataType.MultilineText)]
         public string LiveShowHtml { get; set; }
 
-        [Display(Name = "Next Show Date/time", Description = "Exact date and time of the next live show")]
+        [Display(Name = "Next Show Date/time", Description = "Exact date and time of the next live show in Pacific Time")]
         [DateAfterNow(TimeZoneId = "Pacific Standard Time")]
         public DateTime? NextShowDatePst { get; set; }
 
