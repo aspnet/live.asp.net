1d6362750c912c89f3d20a8f271f43b8b068a316
DamianEdwards <damian@damianedwards.com>
2017-01-04 11:05:57 -0800

    More post migrate fixes

--- src/live.asp.net/Startup.cs
+++ src/live.asp.net/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
